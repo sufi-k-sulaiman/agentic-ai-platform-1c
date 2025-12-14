133d80a82c7ec54e579dd71735ce7c518552738d
Sufi K Sulaiman <ssufikhan@icloud.com>
2025-12-14 08:21:03 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import ScrollToTop from '@/components/ScrollToTop';
 
 export default function Layout({ children }) {
   return (
     <HelmetProvider>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col bg-white">
         <Header />
         <main className="flex-1 pt-16">
