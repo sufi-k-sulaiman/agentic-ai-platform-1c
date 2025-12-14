35f98cd2122f5ab874b48244cdd2c4c1cb0aae42
Sufi K Sulaiman <ssufikhan@icloud.com>
2025-12-14 08:15:01 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col bg-white">
-      <Header />
-      <main className="flex-1 pt-16">
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <HelmetProvider>
+      <div className="min-h-screen flex flex-col bg-white">
+        <Header />
+        <main className="flex-1 pt-16">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </HelmetProvider>
   );
 }
