d4971ca624e1c13441e6a0407be1d52e7e66db4a
Sufi K Sulaiman <ssufikhan@icloud.com>
2025-12-14 06:40:47 +0000

    File changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -103,7 +103,7 @@ export default function Header() {
                         <NavigationMenuTrigger className="text-gray-600 hover:text-[#8B2EE5] bg-transparent hover:bg-transparent data-[state=open]:bg-transparent font-medium text-sm transition-all duration-200 hover:-translate-y-0.5">
                           {item.label}
                         </NavigationMenuTrigger>
-                        <NavigationMenuContent className="data-[motion=from-start]:animate-in data-[motion=from-end]:animate-in data-[motion=to-start]:animate-out data-[motion=to-end]:animate-out fade-in-0 fade-out-0 zoom-in-95 zoom-out-95 slide-in-from-left-1 slide-in-from-right-1">
+                        <NavigationMenuContent className="left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52">
                           <ul className={`grid gap-1 p-4 ${item.label === 'Verticals' ? 'w-[500px] grid-cols-2' : 'w-[400px]'}`}>
                             {item.submenu.map((subitem) => (
                               <li key={subitem.label}>
