dd9314a063068aa2e5b918fb3458ad3b514cffed
Sufi K Sulaiman <ssufikhan@icloud.com>
2025-12-18 09:24:21 +0000

    File changes

--- src/components/home/SolutionsSection.jsx
+++ src/components/home/SolutionsSection.jsx
@@ -224,7 +224,7 @@ const industries = [
 export default function SolutionsSection() {
   const [selectedIndustry, setSelectedIndustry] = useState('Property Management');
   return (
-    <section className="py-16 sm:py-20 lg:py-24 bg-white" id="solutions">
+    <section className="hidden lg:block py-16 sm:py-20 lg:py-24 bg-white" id="solutions">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="grid lg:grid-cols-2 gap-12 lg:gap-16 items-center">
           {/* Left Content */}
